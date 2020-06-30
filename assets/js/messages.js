bb3dc1346d28e381ebab009b0e4f1a4773a19530
Marco Betschart <email@marco.betschart.name>
2020-06-30 21:23:48 +0200

    Initial proof of concept

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,12 +1,20 @@
 var messages = {
     root: document.getElementById("messages"),
+    localeDateStringOptions: {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    },
     add: function(type, text) {
         var className = type ? ("message-" + type) : "message-info";
 
         var element = document.createElement("li");
         element.classList.add("message");
         element.classList.add(className);
-        element.innerHTML = text;
+        element.innerHTML = "[" + (new Date().toLocaleDateString('de-CH', messages.localeDateStringOptions) + "] " + text);
 
         messages.root.appendChild(element);
     },
